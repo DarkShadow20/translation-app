cab27d92c325f734386d06093b98b3639e9eef83
DarkShadow20 <kunal8011@gmail.com>
2020-11-29 13:06:06 +0530

    I/O processing

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var btnTranslate=document.querySelector("#btn-translate")
 var txtInput=document.querySelector('#txt-input');
+var outputDiv=document.querySelector('#txt-output');
+
 
 
 function clickHandler(){
-    console.log("clicked")
-    console.log("input " +txtInput.value)
+    outputDiv.innerText="hello "+ txtInput.value; 
 }
 
 btnTranslate.addEventListener("click",clickHandler)
