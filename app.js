93e0f2ea6c65ecff51b18056dd9c567cd936564a
DarkShadow20 <kunal8011@gmail.com>
2020-11-29 12:46:13 +0530

    take input and console

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var btnTranslate=document.querySelector("#btn-translate")
+var txtInput=document.querySelector('#txt-input');
 
-btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("button clicked");
-})
+
+function clickHandler(){
+    console.log("clicked")
+    console.log("input " +txtInput.value)
+}
+
+btnTranslate.addEventListener("click",clickHandler)
 
